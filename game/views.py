ec07f5b542351f6177d9ac4aa04844a70a68e812
Krzysztof Węgierski <krzysztof.wegierski@huuugegames.com>
2021-01-15 23:30:48 +0100

    Added chat functionality

--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+
 from .models import Token
 
 
 def index(request):
-    return render(request, 'game/gameboard.html', {'token_list': Token.objects.all()})
+    return render(request, 'index.html', {'token_list': Token.objects.all(), 'user': request.user})
