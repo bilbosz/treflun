c621ed65d7addfa1c067bf5668429dc010776e06
Krzysztof Węgierski <krzysztof.wegierski@huuugegames.com>
2021-01-18 11:29:10 +0100

    Code review fixes

--- treflun/asgi.py
+++ treflun/asgi.py
@@ -15,7 +15,6 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'treflun.settings')
 
-# application = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(URLRouter(game.routing.websocket_urlpatterns))
